7232b973a8f7f78683b0d95e8c5c793d614ef27e
23019122- Ervian <23019122@myrp.edu.sg>
2024-11-15 15:50:37 +0800

    Updated Files

--- App.js
+++ App.js
@@ -1,20 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, SectionList, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const datasource = [
+    {
+        data: [
+            { key: 'Muk', picture: require('./img/muk.png') },
+            { key: 'Weezing', picture: require('./img/weezing.png') },
+        ],
+        title: "Dark",
+        bgColor: 'purple',
+        icon: "moon",
+        color: "#000000"
+    },
+    {
+        data: [
+            { key: 'Hitmonchan', picture: require('./img/hitmonchan.png') },
+            { key: 'Machop', picture: require('./img/machop.png') },
+        ],
+        title: "Fighting",
+        bgColor: 'orange',
+        icon: "hand-fist",
+        color: "#704104"
+    },
+    {
+        data: [
+            { key: 'Venomoth', picture: require('./img/venomoth.png') },
+            { key: 'Bulbasaur', picture: require('./img/bulbasaur.png') },
+        ],
+        title: "Grass",
+        bgColor: 'green',
+        icon: "leaf",
+        color: "#73ec83"
+    },
+];
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    opacityStyle: {
+        borderWidth: 1,
+        padding: "auto",
+        marginVertical: 5,
+        borderRadius: 5,
+    },
+    textStyle: {
+        fontSize: 15,
+        margin: 10,
+        textAlign: 'center',
+        flex: 1,
+    },
+    headerText: {
+        fontSize: 20,
+        margin: 10,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        padding: 10,
+    },
+    imageStyle: {
+        width: 200,
+        height: 200,
+        resizeMode: 'contain'
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: 'skyblue',
+        flex: 1,
+        padding: "auto",
+        height: 200,
+    },
+    button: {
+        backgroundColor: 'dodgerblue',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
+
+const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.opacityStyle}>
+        <View style={styles.itemContainer}>
+            <Text style={styles.textStyle}>{item.key}</Text>
+            {item.picture && <Image source={item.picture} style={styles.imageStyle} />}
+        </View>
+    </TouchableOpacity>
+);
+
+
+
+const App = () => {
+
+    return (
+        <View style={{ marginBottom: 50, margin: 10 }}>
+            <StatusBar hidden={true} />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>ADD POKEMON</Text>
+            </TouchableOpacity>
+            <SectionList
+                sections={datasource}
+                keyExtractor={(item) => item.key}
+                renderItem={renderItem}
+                renderSectionHeader={({ section: { title, bgColor, icon, color } }) => (
+                    <View style={[styles.headerContainer, { backgroundColor: bgColor }]}>
+                        <Icon name={icon} size={20} color={color} />
+                        <Text style={[styles.headerText, { color: color }]}>{title}</Text>
+                    </View>
+                )}
+            />
+        </View>
+    );
+};
+
+export default App;
